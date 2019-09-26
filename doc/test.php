c0a60abd9288729853c3c4fe890b752d488c2b44
Matthias <m@tth.es>
2019-09-26 14:20:36 +0200

    fix example

--- doc/test.php
+++ doc/test.php
@@ -6,10 +6,8 @@ namespace Test;
 use Phore\Scheduler\Connector\PhoreSchedulerRedisConnector;
 use Phore\Scheduler\PhoreScheduler;
 
-$redis = new \Redis();
-$redis->connect("redis");
 
-$scheduler = PhoreScheduler::Init(new PhoreSchedulerRedisConnector($redis));
+$scheduler = PhoreScheduler::Init(new PhoreSchedulerRedisConnector("redis"));
 
 $scheduler->defineCommand("test", function ($args) {
     echo "HELLO WORLD!\n";
